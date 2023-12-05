919be0fa53d545e7aee5b96c3b093e490a2435d4
Vladislav Zarovniy <121088289+zvladoss@users.noreply.github.com>
2023-12-06 00:13:33 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function checkForSpam(message) {
+
+  const str = message.toUpperCase();
+
+  return str.includes('SPAM') || str.includes('SALE');
+ 
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
